8e5a1f4eb4bd827533db08a0e09c7cf76308d639
Rainer Müller <raimue@codingfarm.de>
2014-02-16 23:29:18 +0100

    ledtest: First test program

---
+++ arduino/arduino-config.mk
@@ -0,0 +1,7 @@
+BOARD_TAG = pro5v328
+OBJDIR    = .build-$(BOARD_TAG)
+
+MONITOR_PORT = /dev/cu.usb*
+MONITOR_CMD = screen -c /dev/null
+
+ARDMK_DIR = $(realpath ../../tools/arduino-mk)
---
+++ arduino/ledtest/ledtest.ino
@@ -0,0 +1,23 @@
+#define FIRST 8
+#define LAST 13
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Initializing...");
+    for (uint8_t i = FIRST; i <= LAST; i++) {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);
+    }
+    Serial.println("Initialization done!");
+}
+
+void loop() {
+    for (uint8_t i = FIRST; i <= LAST; i++) {
+        digitalWrite(i, HIGH);
+        delay(500);
+    }
+    for (uint8_t i = FIRST; i <= LAST; i++) {
+        digitalWrite(i, LOW);
+        delay(500);
+    }
+}
