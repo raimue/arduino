d974cbb2f1c818ea06c60d2ad63faaed4b6c1828
Rainer Müller <raimue@codingfarm.de>
2014-02-20 22:09:47 +0100

    Whitespace only

--- src/lcd/lcd.cpp
+++ src/lcd/lcd.cpp
@@ -23,9 +23,9 @@ void setup() {
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis()/1000);
 }
