f6abde2146d189ad6b842090b8bedb64c80e2078
Rainer Müller <raimue@codingfarm.de>
2014-04-26 13:55:25 +0200

    Choose monitor port depending on the OS

--- src/arduino-config.mk
+++ src/arduino-config.mk
@@ -1,7 +1,11 @@
 BOARD_TAG = pro5v328
 OBJDIR    = .build/$(BOARD_TAG)
 
+ifeq ($(shell uname -s),Darwin)
 MONITOR_PORT = /dev/cu.usb*
+else
+MONITOR_PORT = /dev/ttyACM*
+endif
 MONITOR_CMD = screen -c /dev/null
 
 ARDMK_DIR = $(realpath ../../tools/arduino-mk)
