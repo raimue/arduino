2a14b88f779fb6e19481ed5b817023ba2cf09f69
Rainer Müller <raimue@codingfarm.de>
2018-11-03 15:08:11 +0100

    Do not override variables in config
    Set variables only if not already set and include Arduino.mk at the end
    to simplify project Makefiles.

--- src/arduino-config.mk
+++ src/arduino-config.mk
@@ -1,17 +1,20 @@
-BOARD_TAG = pro5v328
-OBJDIR    = .build/$(BOARD_TAG)
+BOARD_TAG ?= pro5v328
+OBJDIR    ?= .build/$(BOARD_TAG)
 
 ifeq ($(shell uname -s),Darwin)
-MONITOR_PORT = /dev/cu.usb*
+MONITOR_PORT ?= /dev/cu.usb*
 else
-MONITOR_PORT = /dev/ttyACM*
+MONITOR_PORT ?= /dev/ttyACM*
 endif
-MONITOR_CMD = screen -c /dev/null
+MONITOR_CMD ?= screen -c /dev/null
 
-ARDMK_DIR = $(realpath ../../tools/arduino-mk)
+ARDMK_DIR ?= $(realpath ../../tools/arduino-mk)
+ARDMK_FILE ?= $(ARDMK_DIR)/Arduino.mk
 
 # Helpful aliases
 .PHONY: default serial flash
 default: all
 serial: monitor
 flash: upload
+
+include $(ARDMK_FILE)
