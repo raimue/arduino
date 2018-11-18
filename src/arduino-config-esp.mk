46754c049d2451c3bd83b6147b722e1553d963a3
Rainer Müller <raimue@codingfarm.de>
2018-11-18 15:41:48 +0100

    esp01: Reduce upload speed for more reliability

--- src/arduino-config-esp.mk
+++ src/arduino-config-esp.mk
@@ -4,6 +4,6 @@ ARDUINO_DIR = $(realpath ../..)
 ARDMK_DIR ?= $(realpath ../../tools/arduino-mk-esp)
 ARDMK_FILE ?= $(ARDMK_DIR)/ArduinoESP.mk
 
-ESP_UPLOAD_SPEED = 921600
+ESP_UPLOAD_SPEED = 115200
 
 include ../arduino-config.mk
