1de3e49124f93f9c30af2adaee2215e707b91e3c
Rainer Müller <raimue@codingfarm.de>
2021-02-08 13:15:23 +0100

    Arduino-mk: Update to version 1.6.0
    This version of Arduino-mk supports Arduino >= 1.5.x and its new library
    format. As this isn't packaged in Debian/Ubuntu, point to an
    installation in /opt if it exists.

--- src/arduino-config.mk
+++ src/arduino-config.mk
@@ -6,6 +6,10 @@ MONITOR_CMD ?= picocom
 ARDMK_DIR ?= $(realpath ../../tools/arduino-mk)
 ARDMK_FILE ?= $(ARDMK_DIR)/Arduino.mk
 
+ifneq ($(wildcard /opt/arduino),)
+ARDUINO_DIR = $(realpath /opt/arduino)
+endif
+
 # Helpful aliases
 .PHONY: default serial flash
 default: all
