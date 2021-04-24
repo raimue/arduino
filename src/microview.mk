cb99f1eb4663920fd1bfa118b76ed61de5bbf965
Rainer Müller <raimue@codingfarm.de>
2021-04-24 18:26:10 +0200

    microview: Append to ARDUINO_LIBS, don't override

--- src/microview.mk
+++ src/microview.mk
@@ -1,6 +1,6 @@
 BOARDS_TXT = ../../hardware/boards.txt
 BOARD_TAG = uview
-ARDUINO_LIBS = MicroView
+ARDUINO_LIBS += MicroView
 
 ifneq ($(wildcard /dev/ttyUSBMicroView),)
 MONITOR_PORT = /dev/ttyUSBMicroView
