dd340d966fe5899a259feda54ffb7dcc7c7f75df
Rainer Müller <raimue@codingfarm.de>
2021-04-24 15:58:37 +0200

    microview_monswitch: First tests with finished hardware

--- src/microview_monswitch/monswitch.cpp
+++ src/microview_monswitch/monswitch.cpp
@@ -17,27 +17,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <MicroView.h>
 
-MicroViewWidget *widget;
+#define BUTTON0 A1
+#define BUTTON1 A4
+#define SWITCH0 A0
 
 void setup() {
+    // Button 0 as input with pull-up
+    pinMode(BUTTON0, INPUT);
+    digitalWrite(BUTTON0, HIGH);
+
+    // Button 1 as input with pull-up,
+    // GND provided by pin 2
+    pinMode(BUTTON1, INPUT);
+    digitalWrite(BUTTON1, HIGH);
+    pinMode(A5, OUTPUT);
+    digitalWrite(A5, LOW);
+
+    // Switch 0 as input with pull-up
+    digitalWrite(SWITCH0, INPUT);
+    digitalWrite(SWITCH0, HIGH);
+
+    // MicroView init
     uView.begin();
     uView.flipHorizontal(true);
     uView.flipVertical(true);
-    widget = new MicroViewGauge(32, 24, 0, 1023, WIDGETSTYLE0);
     uView.clear(PAGE);
 }
 
 void loop() {
-    int sense = A0;
-    pinMode(sense, INPUT);
-    digitalWrite(sense, HIGH);
-
-    int res = analogRead(sense);
-
+    uView.setCursor(0, 0);
     uView.clear(PAGE);
-    widget->reDraw();
-    widget->setValue(res);
+    uView.print("HelloWorld");
+    uView.print(digitalRead(BUTTON0));
+    uView.print(digitalRead(BUTTON1));
+    uView.print(digitalRead(SWITCH0));
     uView.display();
 
-    delay(10);
+    delay(100);
 }
